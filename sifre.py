ea15454a6ea7c973e857d79572da6d5eeb36a4e4
Gökhan:Yazılım/GameDesign <93435432+Carnage-TR@users.noreply.github.com>
2022-06-04 15:20:47 +0300

    Add files via upload

---
+++ sifre.py
@@ -0,0 +1,7 @@
+#       {Dosyayı Çözmek}
+
+#1[Key'i al]
+#2[Şifre çöz fonksiyonunu yaz]
+#3[Şifreyi çöz-aynı isimle dosyayı kaydet]
+
+#NOT: w kipiyle aç, böylece eski bilgileri silinip yeni bilgiler yazılıur.
